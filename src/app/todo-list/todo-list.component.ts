220b32986897683ad0d0fd5008b61bd81c17b0fd
Naszradi Mihály <namtaai@INF.ELTE.HU>
2017-10-19 15:33:45 +0200

    first commit

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+
+  todos: Array<string> = [
+    'valami',
+    'valami2',
+    'Kivinni a szemetet',
+    'ALMA ALMA'
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
